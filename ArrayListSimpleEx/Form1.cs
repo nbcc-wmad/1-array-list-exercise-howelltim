f906be0eb06864abb78ae20514099b7884c77157
Tehqq <th90@ymail.com>
2019-04-23 12:18:26 -0300

    Trying to get this to work
    Test commit

--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace ArrayListSimpleEx
 {
@@ -15,6 +16,19 @@ namespace ArrayListSimpleEx
         public Form1()
         {
             InitializeComponent();
+        }
+        private ArrayList message;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            message.Add("I");
+            message.Add("Love");
+            message.Add("Programming");
+            message.Add("So");
+            message.Add("Much");
+        }
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
